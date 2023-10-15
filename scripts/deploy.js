31aee036c6b8feea56ad8006885cb2163f54cc02
kohuyso <90167719+kohuyso@users.noreply.github.com>
2023-10-15 22:20:09 +0700

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Oasis_NFTMarketplace = await hre.ethers.getContractFactory(
+    "Oasis_NFTMarketplace"
+  );
+  const oasis_NFTMarketplace = await Oasis_NFTMarketplace.deploy();
+  await oasis_NFTMarketplace.deployed();
+  console.log(oasis_NFTMarketplace);
+  console.log(`Contract address ${oasis_NFTMarketplace.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
